d9d6e07eaaecf895ce295f77ee6a4187b15702f2
sushant kumar singh <81570152+sushant13042002@users.noreply.github.com>
2022-09-23 03:04:33 +0530

    Add files via upload

---
+++ binary_no_check.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+class binary
+{
+    string s;
+public:
+    void read(void);
+    void chk_bin(void);
+    void ones_compliment(void);
+    void display(void);
+};
+
+void binary ::read(void){
+
+    cout<<"enter binary number : "<< endl;
+    cin >>s;
+}
+
+void binary ::chk_bin(void)
+{
+    for(int i = 0;i < s.length(); i++)
+    {
+        if(s.at(i) != '0' && s.at(i) != '1')
+        {
+            cout<<"Not valid"<<endl;
+            exit(0);
+        }
+    }
+}
+
+void binary::ones_compliment(void)
+{
+    for(int i = 0; i<s.length(); i++)
+    {
+        if(s.at(i)=='0'){
+            s.at(i)= '1';
+        }
+        else
+        {
+            s.at(i) = '0';
+        }
+    }
+}
+
+void binary::display(void)
+{
+    cout<<"Dispaly Number:  "<<endl;
+    for(int i = 0; i< s.length();i++)
+    {
+        cout<<s.at(i);
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+    binary b;
+    b.read();
+    b.chk_bin();
+    b.ones_compliment();
+    b.display();
+
+    return 0;
+}
