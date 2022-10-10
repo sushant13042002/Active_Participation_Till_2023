90a03fea63c54ccb52a28d61a1b73a75a9ebb68f
Sushant <sushant13042002@gmail.com>
2022-10-10 21:11:43 +0530

    added sub function

--- add 2 number.cpp
+++ add 2 number.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int a , b;
 int sum;
+int sub;
 int main(){
     cout<<"hello";
     cin>>a>>b;
     sum = a+b;
+    sub = a-b;
 
     cout<<"a + b = "<<sum;
+    cout<<"a - b = "<<sub; 
     return 0;
 }
