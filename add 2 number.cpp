7ec4282f9c1a54f4135d1b7d53fceb01f58e9642
Devashish <devashishkrishna@gmail.com>
2022-10-11 19:01:55 +0530

    added modulus function

--- add 2 number.cpp
+++ add 2 number.cpp
@@ -5,6 +5,7 @@ int sum;
 int sub;
 int mul;
 int divi;
+int mod;
 int main(){
     cout<<"Silent Chora";
     cin>>a>>b;
@@ -12,10 +13,12 @@ int main(){
     sub = a-b;
     mul = a*b;
     divi = a/b;
+    mod=a%b;
 
     cout<<"a + b = "<<sum;
     cout<<"a - b = "<<sub; 
     cout<<"a * b = "<<mul;
-    cout<<"a / b = "<<divi; 
+    cout<<"a / b = "<<divi;
+    cout<<"a % b = "<<mod ;
     return 0;
 }
